5befacc5db1c9be11d5ef2a7dee2ac87a6faf3b8
ultting <hard7762@naver.com>
2022-01-30 23:21:31 +0900

    VS Code git con

--- Django.py
+++ Django.py
@@ -1,1 +1,9 @@
+# git 설치 후 내 git id와 name 을 입력
+# 명령어는 git config -- global user.email "my email @ xxxx.com"
+# git config -- global user.name "my nick"
+
+# git에 push 하는 법
+# Source Control -> + 클릭 -> Commit 메세지 -> Check 표시 
+
 print('Hello Django')
+print('VS Code 와 Git hub 연동 테스트')
